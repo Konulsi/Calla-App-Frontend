5913e69f16140c7946e6cf4f9849fbcf11354a19
Konulsi <konulsi@code.edu.az>
2023-06-01 19:40:09 +0400

    initial

---
+++ assets/js/home.js
@@ -0,0 +1,71 @@
+// //slider
+
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+
+//product slider
+$(document).ready(function(){
+    $('.product-cards').slick({
+        dots: true,
+        infinite: false,
+        speed: 700,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+
+        ]
+      });
+
+
+
+
+    //   $(".product-card").on("mouseover",function(){
+    //     $(".product-img-hover").removeClass("d-none");
+
+    //   })
+    //   $(".product-card").on("mouseout",function(){
+    //     $(".product-img-hover").addClass("d-none");
+
+    //   })
+})
+   
+
